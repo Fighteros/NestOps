33682fd2cf30787c106a8e3a81d1cf7548ce99a8
Ahmed Abd ElGhany <am5419@fayoum.edu.eg>
2025-11-27 02:32:18 +0200

    feat: Create initial NestJS AppModule with global setup and ignore the uploads directory.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
 import { join } from 'path';
 import { LoggerModule } from './common/logger/logger.module';
 import { ConfigModule } from './config/config.module';
+import { RequestContextModule } from './core/context/request-context.module';
 import { GlobalExceptionFilter } from './core/filters/global-exception.filter';
 import { HeaderValidatorInterceptor } from './core/interceptors/header-validator/header-validator.interceptor';
 import { RequestIdInterceptor } from './core/interceptors/request-id/request-id.interceptor';
 import { LoggingInterceptor } from './core/interceptors/response/logging.interceptor';
-import { RequestContextModule } from './core/context/request-context.module';
 import { ResponseInterceptor } from './core/interceptors/response/response.interceptor';
 import { ValidationPipe } from './core/pipes/validation.pipe';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { DocsModule } from './docs/docs.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { UploadsModule } from './modules/uploads/uploads.module';
@@ -35,6 +36,11 @@ import { UploadsModule } from './modules/uploads/uploads.module';
         limit: 120,
       },
     ]),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
 
     ConfigModule,
     LoggerModule,
